6387f5d78b8f2d4aea99fc65383a0e88fa7a5302
narcissedesire <narcissedesire5@gmail.com>
2025-02-15 22:15:14 +0300

    push

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+// import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// sgMail.setApiKey(process.env.API_KEY);
+
+app.post("/api/send", async (req, res) => {
+  const { email, sujet, message } = req.body;
+
+  const msg = {
+    to: process.env.TO_EMAIL,
+    from: process.env.FROM_EMAIL,
+    subject: sujet,
+    text: message,
+    html: `<p><strong>Source : </strong>${email} <br/></p>
+          <p><strong>${sujet}</strong></p>
+          <p>${message.replace(/\n/g, "<br />")}</p>`,
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log("E-mail envoyé avec succès");
+    res.status(200).json({ status: 200, message: "E-mail envoyé !" });
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'e-mail :", error);
+    res
+      .status(500)
+      .json({ status: 500, message: "Erreur lors de l'envoi de l'e-mail" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
