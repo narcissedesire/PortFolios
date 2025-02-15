0e1a460e9c74120a62573ec91289fe01c4485868
narcissedesire <116617142+narcissedesire@users.noreply.github.com>
2025-02-15 23:20:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-// import sgMail from "@sendgrid/mail";
+import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
 dotenv.config();
 
