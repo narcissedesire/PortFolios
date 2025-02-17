9e679df920ac7bc6f6b77dd9fcdeb6e62aadb644
narcissedesire <116617142+narcissedesire@users.noreply.github.com>
2025-02-17 18:10:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import sgMail from "@sendgrid/mail";
-import dotenv from "dotenv";
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// app.use(cors());
-app.use(cors({
-  origin: "https://port-folio-narcisse.vercel.app",
-}));
-app.use(bodyParser.json());
-
-sgMail.setApiKey(process.env.API_KEY);
-
-app.post("/api/send", async (req, res) => {
-  const { email, sujet, message } = req.body;
-
-  const msg = {
-    to: process.env.TO_EMAIL,
-    from: process.env.FROM_EMAIL,
-    subject: sujet,
-    text: message,
-    html: `<p><strong>Source : </strong>${email} <br/></p>
-          <p><strong>${sujet}</strong></p>
-          <p>${message.replace(/\n/g, "<br />")}</p>`,
-  };
-
-  try {
-    await sgMail.send(msg);
-    console.log("E-mail envoyé avec succès");
-    res.status(200).json({ status: 200, message: "E-mail envoyé !" });
-  } catch (error) {
-    console.error("Erreur lors de l'envoi de l'e-mail :", error);
-    res
-      .status(500)
-      .json({ status: 500, message: "Erreur lors de l'envoi de l'e-mail" });
+{
+  "name": "back",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "build": "npm install",
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": "",
+  "dependencies": {
+    "@sendgrid/mail": "^8.1.4",
+    "body-parser": "^1.20.3",
+    "cors": "^2.8.5",
+    "dotenv": "^16.4.7",
+    "express": "^4.21.2",
+    "itty-router": "^5.0.18",
+    "nodemon": "^3.1.9",
+    "undici": "^7.3.0",
+    "worktop": "^0.7.3"
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
-});
+}
