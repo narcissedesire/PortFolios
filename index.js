def6fdeb542cc1b9bd14fc410d0daa42d1c5728f
narcissedesire <116617142+narcissedesire@users.noreply.github.com>
2025-02-17 20:12:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
-import { Router } from "itty-router";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Initialise le routeur
-const router = Router();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Configurer l'API de SendGrid
+app.use(cors());
+app.use(bodyParser.json());
+
 sgMail.setApiKey(process.env.API_KEY);
 
-// Définir la route pour envoyer l'email
-router.post("/api/send", async (req) => {
-  const { email, sujet, message } = await req.json();
+app.post("/api/send", async (req, res) => {
+  const { email, sujet, message } = req.body;
 
   const msg = {
     to: process.env.TO_EMAIL,
     from: process.env.FROM_EMAIL,
     subject: sujet,
     text: message,
-    html: `<p><strong>Source: </strong>${email} <br/></p>
-           <p><strong>${sujet}</strong></p>
-           <p>${message.replace(/\n/g, "<br />")}</p>`,
+    html: `<p><strong>Source : </strong>${email} <br/></p>
+          <p><strong>${sujet}</strong></p>
+          <p>${message.replace(/\n/g, "<br />")}</p>`,
   };
 
   try {
     await sgMail.send(msg);
-    return new Response(
-      JSON.stringify({ status: 200, message: "E-mail envoyé !" }),
-      {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
+    console.log("E-mail envoyé avec succès");
+    res.status(200).json({ status: 200, message: "E-mail envoyé !" });
   } catch (error) {
     console.error("Erreur lors de l'envoi de l'e-mail :", error);
-    return new Response(
-      JSON.stringify({
-        status: 500,
-        message: "Erreur lors de l'envoi de l'e-mail",
-      }),
-      {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
+    res
+      .status(500)
+      .json({ status: 500, message: "Erreur lors de l'envoi de l'e-mail" });
   }
 });
 
-// Gérer la requête entrante et répondre avec le router
-addEventListener("fetch", (event) => {
-  event.respondWith(router.handle(event.request));
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
 });
